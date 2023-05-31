560be396ad50d750c657c1c4afe23555c5a88a03
saishkulkarni <saishkulkarni7@gmail.com>
2023-05-31 11:07:30 +0530

    Added some htmls

--- src/main/java/dao/MyDao.java
+++ src/main/java/dao/MyDao.java
@@ -1,10 +1,13 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import dto.Doctor;
 import dto.Staff;
 
 public class MyDao {
@@ -16,5 +19,51 @@ public class MyDao {
 		transaction.begin();
 		manager.persist(staff);
 		transaction.commit();
+	}
+	
+	public void saveDoctor(Doctor doctor) {
+		transaction.begin();
+		manager.persist(doctor);
+		transaction.commit();
+	}
+
+	public Staff fetchStaff(long mobile) {
+		List<Staff> list = manager.createQuery("select x from Staff x where mobile=?1").setParameter(1, mobile)
+				.getResultList();
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+	}
+
+	public Staff fetchStaff(String email) {
+		List<Staff> list = manager.createNativeQuery("select * from staff where email=?1", Staff.class)
+				.setParameter(1, email).getResultList();
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+	}
+
+	public Doctor fetchDoctor(long mobile) {
+		List<Doctor> list = manager.createQuery("select x from Doctor x where mobile=?1").setParameter(1, mobile)
+				.getResultList();
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+	}
+
+	public Doctor fetchDoctor(String email) {
+		List<Doctor> list = manager.createNativeQuery("select * from doctor where email=?1", Doctor.class)
+				.setParameter(1, email).getResultList();
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(0);
+		}
 	}
 }
