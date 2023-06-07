23baef5d90091e15a032d001cabf84eac0f51d2e
saishkulkarni <saishkulkarni7@gmail.com>
2023-06-07 09:25:51 +0530

    2nd commit for the project

--- src/main/java/controller/FetchAllStaff.java
+++ src/main/java/controller/FetchAllStaff.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.MyDao;
+import dto.Staff;
+
 @WebServlet("/fetchallstaff")
 public class FetchAllStaff extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		MyDao dao=new MyDao();
+		List<Staff> list=dao.fetchAllStaff();
 	}
 }
